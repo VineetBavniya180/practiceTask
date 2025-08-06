da1d9f29cdcba5749a2fb01f86ce6fc2475440aa
VineetBavniya180 <vineet.bavniya@clouddrove.com>
2025-08-06 15:40:30 +0530

    One Try 3

---
+++ createConflict.sh
@@ -0,0 +1,21 @@
+#!/bin/bash 
+
+# to check branch 
+branchName="$(git branch | grep -i "*" | awk '{print $2}')"
+echo "$branchName"
+if [$branchName != "forMerge"]; then 
+    echo "please change branch and save code in forMerge branch";
+else
+    echo "Name: HarshalSir" > file1
+    git add .
+    git commit -m "One Try 3"
+    git push origin forMerge 
+
+    git checkout main 
+    echo "Name: NikhilSir" > file1
+    git add .
+    git commit -m "create conflict another try 4"
+    git push origin main    
+
+fi
+
